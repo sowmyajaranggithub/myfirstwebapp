71bef6f8f86e5865d830263cfd8131c336615998
sowmyajaranggithub <sowmyajarang1@gmail.com>
2023-07-23 22:40:37 +0530

    V 0.4

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
@@ -1,43 +1,65 @@
 package com.in28minutes.springboot.myfirstwebapp.todo;
 
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
 public class Todo {
+    public Todo(int id, String username, String description, LocalDate targetDate, boolean done) {
+        this.id = id;
+        this.username = username;
+        this.description = description;
+        this.targetDate = targetDate;
+        this.done = done;
+    }
+
     private int id;
-    private String username;
-    private String description;
-    private LocalDate targetDate;
-    private boolean done;
+  private String username;
+  private String description;
+  private LocalDate targetDate;
+  private boolean done;
+
+
     public int getId() {
         return id;
     }
-    public LocalDate getTargetDate() {
-        return targetDate;
-    }
-    public void setTargetDate(LocalDate targetDate) {
-        this.targetDate = targetDate;
-    }
-    public boolean isDone() {
-        return done;
-    }
-    public void setDone(boolean done) {
-        this.done = done;
-    }
-    public String getUsername() {
-        return username;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
+
     public void setId(int id) {
         this.id = id;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getTargetDate() {
+        return targetDate;
+    }
+
+    public void setTargetDate(LocalDate targetDate) {
+        this.targetDate = targetDate;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
     @Override
     public String toString() {
         return "Todo{" +
@@ -48,15 +70,5 @@ public class Todo {
                 ", done=" + done +
                 '}';
     }
-
-    public Todo(int id, String username, String description, LocalDate targetDate, boolean done) {
-        super();
-        this.id = id;
-        this.username = username;
-        this.description = description;
-        this.targetDate = targetDate;
-        this.done = done;
-    }
-
 }
 
--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -1,9 +1,11 @@
 package com.in28minutes.springboot.myfirstwebapp.todo;
 
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,13 +28,20 @@ public class TodoController {
         return "listTodos";
     }
     @RequestMapping(value="add-todo", method = RequestMethod.GET)
-    public String showNewTodoPage(){
+    public String showNewTodoPage(ModelMap model){
+        String username=(String)model.get("name");
+        Todo todo = new Todo(0,username,"Default Desc",LocalDate.now().plusYears(1),false);
+        model.put("todo", todo);
         return "todo";
     }
     @RequestMapping(value="add-todo", method = RequestMethod.POST)
-    public String addNewTodo(@RequestParam String description,ModelMap model) {
+    public String addNewTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+        if (result.hasErrors()){
+            return "todo";
+        }
         String username=(String)model.get("name");
-    todoService.addTodo(username, description, LocalDate.now().plusYears(1), false);
+    todoService.addTodo(username, todo. getDescription(),
+            LocalDate.now().plusYears(1), false);
         return "redirect:list-todos";
     }
 }
